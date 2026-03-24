38789d069691984e2c9e4ac9231df5a799f5af3c
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-03-24 20:13:53 -0300

    upd

--- src/main/java/br/com/justacoder/easyjava/Easy.java
+++ src/main/java/br/com/justacoder/easyjava/Easy.java
@@ -1,1 +1,39 @@
+package br.com.justacoder.easyjava; 
+import java.util.Scanner;
+public class Easy{
+    private static Scanner leitor = new Scanner(System.in);
 
+    // Seu método print (usando System.out em vez de err para cor padrão)
+    public static void print(Object printar) {
+        System.out.println(printar);
+    }
+
+    // Seu scanf estilo C
+    public static Object scanf(String tipo) {
+        if (tipo.equals("i")) {
+            int coisa = leitor.nextInt();
+            leitor.nextLine();
+            return coisa;
+        }
+        if (tipo.equals("d")) {
+            double coisa = leitor.nextDouble();
+            leitor.nextLine();
+            return coisa;
+        }
+        if (tipo.equals("s")) {
+            return leitor.next();
+        }
+        return null;
+    }
+
+    public static String fgets() {
+        return leitor.nextLine();
+    }
+    public static int strlen(String s) {
+        return s.length();
+    }
+
+    public static boolean strcmp(String s1, String s2) {
+        return s1.equals(s2);
+    }
+}
